bb7ddd44ebdf405fd8eaa438f4aeb9bde21a9250
Utkarsh Verma <utkarshverma@protonmail.com>
2021-06-08 15:06:11 +0530

    bootloader: Fix code and add the Fibonacci program

--- atmega328-bootloader/atmega328-bootloader.ino
+++ atmega328-bootloader/atmega328-bootloader.ino
@@ -3,39 +3,59 @@
 #define MI 10
 #define CLK 11
 #define RI 12
-#define EEPROM_CE 13
+#define HLT 13
 #define RST A0
+#define EEPROM_CE A1
 
 #define MASK(pin) (1 << (pin - 8))
 
+// Fibonacci program
 const byte instructions[] = {
-  0b00100100,
-  0b11100000,
-  0b01100000,
-  0b11110000,
-  0b00000001
+  0x50,
+  0x4e,
+  0x51,
+  0x4f,
+  0x1e,
+  0x4d,
+  0x1f,
+  0x4e,
+  0x2d,
+  0x4f,
+  0x70,
+  0xe0,
+  0x64,
+  0xf0
 };
 
 void setup() {
   // Set pins to output
-  DDRD |= 0b11111100;                                                     // D2 - D7
-  DDRB = 0b00000011 | MASK(EEPROM_CE) | MASK(MI) | MASK(RI) | MASK(CLK);  // D8 - D9, RI, CLK, MI, EEPROM_CE
+  DDRD |= 0b11111100;                                               // D2 - D7
+  DDRB = 0b00000011 | MASK(HLT) | MASK(MI) | MASK(RI) | MASK(CLK);  // D8 - D9, RI, CLK, MI, HLT
+  pinMode(RST, OUTPUT);
+  pinMode(EEPROM_CE, OUTPUT);
+
+  // Halt the computer
+  digitalWrite(HLT, HIGH);
 
   // Disable ucode EEPROMs
   digitalWrite(EEPROM_CE, HIGH);
 
+  // Write instructions to RAM
   for (int i = 0; i < LEN(instructions); i++) {
     setAddress(i);
     writeRAM(instructions[i]);
   }
 
-  // Set pins to floating(input)
-  DDRD &= 0b00000011;
-  DDRB &= MASK(EEPROM_CE);
-
   // Reset the computer
-  pinMode(RST, OUTPUT);
   pulse(RST, HIGH);
+
+  // Unhalt the computer
+  digitalWrite(HLT, LOW);
+
+  // Set pins to input/floating
+  DDRD &= 0b00000011;  // D2 - D7
+  DDRB = 0;            // D8 - D13
+  pinMode(RST, INPUT);
 
   // Enable ucode EEPROMs
   digitalWrite(EEPROM_CE, LOW);
@@ -51,6 +71,8 @@ void pulse(int pin, bool signal) {
 
 void setAddress(byte address) {
   PORTD = address << 2;
+  PORTB &= MASK(HLT);
+
   digitalWrite(MI, HIGH);
   pulse(CLK, HIGH);
   digitalWrite(MI, LOW);
@@ -58,7 +80,7 @@ void setAddress(byte address) {
 
 void writeRAM(byte value) {
   PORTD = value << 2;
-  PORTB = (value & 0b11000000) >> 6 | MASK(EEPROM_CE);
+  PORTB = (value >> 6) | (PORTB & MASK(HLT));
 
   digitalWrite(RI, HIGH);
   pulse(CLK, HIGH);
